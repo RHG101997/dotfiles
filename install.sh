b2627d83ff572ed4e9996ee8b3c9b546a5e35602
Richard Hernandez <3848345+RHG101997@users.noreply.github.com>
2021-03-29 16:36:56 -0400

    sgNew alias

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #Remove necesary files
-rm ~/.bashrc
 rm ~/.vimrc
 
 #Link the files to home
 ln ./.vimrc ~/.vimrc
 ln ./.gitconfig ~/.gitconfig
+ln ./.bash_aliases ~/.bash_aliases
 
 #Install plug for vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
